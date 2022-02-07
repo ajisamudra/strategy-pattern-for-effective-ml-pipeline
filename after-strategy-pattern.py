7a9c433ce8a35468929c415ee7b6647040848c98
ajisamudra <aji.samudra@traveloka.com>
2022-02-07 16:10:33 +0700

    add context

--- after-strategy-pattern.py
+++ after-strategy-pattern.py
@@ -9,7 +9,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 
 
-class LearningAlgorithm(ABC):
+class Algorithm(ABC):
     @abstractmethod
     def fit(self, X_train: pd.DataFrame, y_train: pd.Series) -> None:
         # the implementation will be defined in derived class
@@ -21,7 +21,7 @@ class LearningAlgorithm(ABC):
         pass
 
 
-class SklearnLogReg(LearningAlgorithm):
+class LogReg(Algorithm):
     def __init__(self, *args, **kwargs) -> None:
         self.__model = LogisticRegression(*args, **kwargs)
         self.__scaler = StandardScaler()
@@ -31,6 +31,7 @@ class SklearnLogReg(LearningAlgorithm):
         X_train = self.__scaler.fit_transform(X_train)
         # fit to logistic regression
         self.__model.fit(X_train, y_train)
+        return self
 
     def predict(self, X_test: pd.DataFrame) -> np.ndarray:
         # scale features on X_test
@@ -38,7 +39,7 @@ class SklearnLogReg(LearningAlgorithm):
         return self.__model.predict(X_test)
 
 
-class LgbmClassifier(LearningAlgorithm):
+class GradientBoosting(Algorithm):
     def __init__(self, *args, **kwargs) -> None:
         self.__model = LGBMClassifier(*args, **kwargs)
 
@@ -57,12 +58,39 @@ class LgbmClassifier(LearningAlgorithm):
                 log_evaluation(0),
             ],
         )
+        return self
 
     def predict(self, X_test: pd.DataFrame) -> np.ndarray:
         return self.__model.predict(X_test)
 
 
-def train_pipeline(algorithm: LearningAlgorithm) -> None:
+class Pipeline:
+    def __init__(self, algorithm: Algorithm, X_train, X_test, y_train, y_test) -> None:
+        self.__algorithm = algorithm
+        self.__X_train = X_train
+        self.__X_test = X_test
+        self.__y_train = y_train
+        self.__y_test = y_test
+
+    def train(self) -> None:
+        # fit the algorithm
+        self.__algorithm.fit(self.__X_train, self.__y_train)
+
+        # get training evaluation score
+        y_train_pred = self.__algorithm.predict(self.__X_train)
+        train_score = accuracy_score(self.__y_train, y_train_pred)
+
+        # get test evaluation score
+        y_test_pred = self.__algorithm.predict(self.__X_test)
+        test_score = accuracy_score(self.__y_test, y_test_pred)
+
+        # print train and test evaluation score
+        print(
+            f"{type(self.__algorithm).__name__} | train score: {train_score:.5f} | test score: {test_score:.5f}"
+        )
+
+
+def train_pipeline(algorithm: Algorithm) -> None:
     # get Iris data in DataFrame
     X, y = datasets.load_iris(return_X_y=True, as_frame=True)
 
@@ -71,28 +99,14 @@ def train_pipeline(algorithm: LearningAlgorithm) -> None:
         X, y, test_size=0.2, random_state=123
     )
 
-    # fit the algorithm
-    model = algorithm
-    model.fit(X_train, y_train)
-
-    # get training evaluation score
-    y_train_pred = model.predict(X_train)
-    train_score = accuracy_score(y_train, y_train_pred)
-
-    # get test evaluation score
-    y_test_pred = model.predict(X_test)
-    test_score = accuracy_score(y_test, y_test_pred)
-
-    # print train and test evaluation score
-    print(
-        f"{type(algorithm).__name__} | train score: {train_score:.5f} | test score: {test_score:.5f}"
-    )
+    pipeline = Pipeline(algorithm, X_train, X_test, y_train, y_test)
+    pipeline.train()
 
 
 if __name__ == "__main__":
-    algorithm1 = SklearnLogReg()
+    algorithm1 = LogReg()
     train_pipeline(algorithm1)
-    algorithm2 = LgbmClassifier(n_estimators=50)
+    algorithm2 = GradientBoosting(n_estimators=50)
     train_pipeline(algorithm2)
     algorithm3 = "logistic_regression"
     train_pipeline(algorithm3)  # this will throw error
